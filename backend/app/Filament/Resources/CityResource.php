d4f56d4eb659820258ae0ae7126d7177654343b8
Abizar Ar Rifa'i <abizararrifai@gmail.com>
2024-12-19 14:08:22 +0700

    membuat crud pada Office Space Resources

--- backend/app/Filament/Resources/CityResource.php
+++ backend/app/Filament/Resources/CityResource.php
@@ -47,7 +47,9 @@ class CityResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
